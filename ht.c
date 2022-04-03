22c46b0ac73e0cc2c0c3cb652fd9008421328ee6
Aaron Lain <aaron.joel.lain@gmail.com>
2022-04-02 19:30:54 -0500

    getter

--- ht.c
+++ ht.c
@@ -1,3 +1,6 @@
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
 typedef struct {
     const char* key;
     void* value;
@@ -8,6 +11,16 @@ struct ht {
     size_t capacity;
     size_t length;
 };
+
+static uint64_t hash_key(const char* key) {
+    uint64_t hash = FNV_OFFSET;
+    for (const char* p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
 
 ht* ht_create(void) {
     // Allocate space for hash table struct
@@ -39,4 +52,27 @@ void ht_free(ht* table) {
     free(table->entries);
     free(table);
 }
+
+void* ht_get(ht* table, const char* key) {
+    // AND hash with capacity minus 1 to ensurs 
+    // it's within entires array
+    uint64_t hash = hash_key(key);
+    size_t index = 
+    (size_t)(hash & (uint64_t)(table->capacity -1));
+    
+    // Loop till we find an empty entry
+    while (table->entires[index].key != NULL) {
+        if (strcmp(key, table->entries[index].key) =0) {
+        return table->entries[index].value;
+        }
+        // key wasn't here, move to next
+        // linear probing
+        index ++;
+        if (index >= table->capacity) {
+            // at end of entries array, wrap around
+            index = 0;
+        }
+    }
+    
+    return NULL;
 }
